e39236c3be7d7fd865a3051e38992ded1c3e8574
Dan  Machlenkin <danmachlenkin10@gmail.com>
2022-10-01 21:05:00 +0300

    adding eslint + prettier to my project

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
-const app  = require('./app');
 
-
-// console.log(process.env);
-
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
