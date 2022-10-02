c0757eb71ea227ddbd16ad030fea592f190565f0
Dan  Machlenkin <danmachlenkin10@gmail.com>
2022-10-02 10:51:50 +0300

    first time updating the database

--- server.js
+++ server.js
@@ -16,6 +16,38 @@ mongoose
   })
   .then(() => console.log('DB was successfully connected!'));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Camper',
+  price: 197,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error has happened: ' + err);
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App is listening on port ${port}..`);
